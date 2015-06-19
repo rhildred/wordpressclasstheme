7e7646fa50d2e753a6433a918b988150f3dd71c2
rhildred <rhildred@gmail.com>
2015-06-19 12:05:46 -0400

    added menu

--- index.php
+++ index.php
@@ -7,7 +7,34 @@
         <meta name="viewport" content="initial-scale=1, maximum-scale=1">
     </head>
     <body>
-         <div id="main">
+
+        <!-- Nav -->
+            <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+
+
+
+
+         <div id="main" class="container">
              <div id="home">
              <?php $recent = new WP_Query("pagename=home"); while($recent->have_posts()) : $recent->the_post();?>
                 <?php the_content(); ?>
